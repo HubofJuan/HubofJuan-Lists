7cf56b34d5ddb9d3688c7ca1f05c1c1e822f2553
Juan Forero <forerojg@vcu.edu>
2023-03-01 21:44:45 +0000

    completed List exercise lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,5 +1,7 @@
 
+import java.lang.reflect.Array;
 import java.util.List;
+import java.util.*;
 
 /**
  * Lists are ordered collections. This means that we could access elements at a certain position of a list,
@@ -28,8 +30,15 @@ public class Lab {
      *
      * @return a List<Integer> object.
      */
+
+
     public List<Integer> createList(){
-        return null;
+        
+        ///Array g [];
+        List<Integer> ge = new ArrayList<>() ;
+
+        return ge;
+
     }
 
     /**
@@ -39,7 +48,9 @@ public class Lab {
      * @return the size of List (number of items it holds.)
      */
     public int getSize(List<Integer> list){
-        return 0;
+        //getSize = new getSize.index();
+        
+        return list.size();
     }
 
     /**
@@ -50,8 +61,12 @@ public class Lab {
      * @param value an integer that we would like to add to list.
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
-    public void addToList(List<Integer> list, int value){
-    }
+  public void addToList(List<Integer> list, int value){
+    list.add(value);
+}
+        
+
+    
 
     /**
      * Get a particular index of a list.
@@ -65,9 +80,10 @@ public class Lab {
      * @param index represents the index of the element we would like to retrieve.
      * @return the int at the location in 'list' represented by 'index'.
      */
-    public int get(List<Integer> list, int index){
-        return 0;
-    }
+    public int get(List<Integer> list, int index) {
+        int value = list.get(index);
+        return value;}
+    
 
     /**
      * Remove an index from a list.
@@ -79,8 +95,10 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeFromList(List<Integer> list, int position){
+        int valuer = list.remove(position);
+        }
 
-    }
+    
 
     /**
      * Update an index of a list.
@@ -92,5 +110,6 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void updateAtPosition(List<Integer> list, int position, int value){
+        list.set(position, value);
     }
 }
